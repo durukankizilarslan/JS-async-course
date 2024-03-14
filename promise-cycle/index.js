179ac977f8d3b61e6ed0d91d147a7ec2a6563cfb
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-15 02:01:14 +0300

    finally method added

--- promise-cycle/index.js
+++ promise-cycle/index.js
@@ -62,4 +62,8 @@ order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
 
   .catch(() => {
     console.log("customer left");
+  })
+
+  .finally(() => {
+    console.log("Day ended, shop is closed");
   });
