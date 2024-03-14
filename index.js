61bef0fec579e49845ff3c11bee5cabd7faff7bc
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-14 14:59:46 +0300

    Callbacks practiced

--- index.js
+++ index.js
@@ -16,3 +16,29 @@ setTimeout(() => {
   console.log("ice cream ");
 }, 3000);
 console.log("eat ");
+
+// Callbacks: Callbacks are simply calling functions in another function to form a relationship
+
+function one(call_two) {
+  console.log("step 1 completed. Please call step 2");
+  setTimeout(call_two, 3000);
+  call_two();
+}
+
+function two() {
+  console.log("step 2");
+}
+
+one(two);
+
+let order = (call_production) => {
+  console.log("Order placed. Please call production");
+  console.log("Processing...");
+  setTimeout(call_production, 3000);
+};
+
+let production = () => {
+  console.log("Order received, starting production");
+};
+
+order(production);
