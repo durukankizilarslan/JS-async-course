ce290225accfe08d3df2c5057db0a93791a872db
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-14 15:05:00 +0300

    commented out practices

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
 // Synchronous
 
-console.log("I ");
+// console.log("I ");
+// console.log("eat ");
+// console.log("ice cream ");
+// console.log("with a ");
+// console.log("spoon. ");
 
-console.log("eat ");
-console.log("ice cream ");
-console.log("with a ");
-console.log("spoon. ");
+//------------------------------------------------------
 
 // Asynchronous
 
-console.log("I ");
-console.log("with a ");
-console.log("spoon. ");
-setTimeout(() => {
-  console.log("ice cream ");
-}, 3000);
-console.log("eat ");
+// console.log("I ");
+// console.log("with a ");
+// console.log("spoon. ");
+// setTimeout(() => {
+//   console.log("ice cream ");
+// }, 3000);
+// console.log("eat ");
+
+//------------------------------------------------------
 
 // Callbacks: Callbacks are simply calling functions in another function to form a relationship
 
-function one(call_two) {
-  console.log("step 1 completed. Please call step 2");
-  setTimeout(call_two, 3000);
-  call_two();
-}
+// function one(call_two) {
+//   console.log("step 1 completed. Please call step 2");
+//   setTimeout(call_two, 3000);
+//   call_two();
+// }
 
-function two() {
-  console.log("step 2");
-}
+// function two() {
+//   console.log("step 2");
+// }
 
-one(two);
+// one(two);
 
-let order = (call_production) => {
-  console.log("Order placed. Please call production");
-  console.log("Processing...");
-  setTimeout(call_production, 3000);
-};
+// let order = (call_production) => {
+//   console.log("Order placed. Please call production");
+//   console.log("Processing...");
+//   setTimeout(call_production, 3000);
+// };
 
-let production = () => {
-  console.log("Order received, starting production");
-};
+// let production = () => {
+//   console.log("Order received, starting production");
+// };
 
-order(production);
+// order(production);
+
+//----------------------------------------------------------
