79e184203a6d76b7a412810899cd453322ff7109
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-14 14:37:07 +0300

    setTimeout practiced

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ console.log("eat ");
 console.log("ice cream ");
 console.log("with a ");
 console.log("spoon. ");
+
+// Asynchronous
+
+console.log("I ");
+console.log("with a ");
+console.log("spoon. ");
+setTimeout(() => {
+  console.log("ice cream ");
+}, 3000);
+console.log("eat ");
